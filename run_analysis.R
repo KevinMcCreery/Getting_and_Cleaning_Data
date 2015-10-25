1f0d7d6a65d70b8d98945cb173c0fb7ea06d6ddb
Kevin McCreery <kevin.mccreery.pub@gmail.com>
2015-10-25 16:58:36 -0400

    NEW FILES

---
+++ run_analysis.R
@@ -0,0 +1,104 @@
+#Purpose: to clean, arrange and otherwisse prepare datasets for analysis,
+#based on instruction provided with the Coursera Course "Getting and Cleaning Data" Project
+
+#"You should create one R script called run_analysis.R that does the following:
+
+#Merges the training and the test sets to create one data set.
+#Extracts only the measurements on the mean and standard deviation for each measurement. 
+#Uses descriptive activity names to name the activities in the data set
+#Appropriately labels the data set with descriptive variable names. 
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject."
+
+
+#Created by: Kevin McCreery for a Coursera Course from Johns Hopikins, October 2015
+
+
+
+#Download of material required to recreate: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+setwd("C:/Users/Kevin/Documents/R/Coursera Files/GettingCleaningData/UCI HAR Dataset/")
+
+library(dplyr)
+library(reshape2)
+#library(RMySQL)
+#library(DBI)
+
+#get the IDs of the subjects of the observations
+subtraining <- read.table("./train/subject_train.txt")
+
+#get the features for col names
+features <- read.table("./features.txt")
+
+#read in the training data with features as col names???
+train <- read.table("./train/X_train.txt", col.names = features[,2])
+
+#Get the activity labels of each of the observations for the training set
+ytrain <- read.table("./train/y_train.txt")
+
+#col bind the subtraining as the row / subject ID col to the train data
+train2 <- cbind(subtraining, ytrain, train)
+#maybe do that in one step using the earlier read.table?
+
+
+
+#get the IDs of the subjects of the observations
+subtesting <- read.table("./test/subject_test.txt")
+
+#read in the testing data with features as col names???
+test <- read.table("./test/X_test.txt", col.names = features[,2])
+
+#Get the activity labels of each of the observations for the testing set
+ytest <- read.table("./test/y_test.txt")
+
+#col bind the subtraining as the row / subject ID col to the test data
+test2 <- cbind(subtesting, ytest, test)
+#maybe do that in one step using the earlier read.table?
+
+
+#I don't think we want to "Merge" these by joining at all. 
+#I think we want to append them to have a complete set of subjects? No?
+combined <- rbind(test2, train2)
+
+#Identify the columns that contain mean or std measurements, 
+#and create a combined logical vector with the desired columns
+
+
+#I was trying to make sure I carried through the necessary first two columns, that don't have mean or std in name
+idcol <- c(1,1, rep.int(0,561))
+
+#Break out the desired column positions into vectors and combine, in order to subset
+meancol <- as.numeric(grepl("mean", colnames(combined)))
+stdcol <- as.numeric(grepl("std", colnames(combined)))
+desiredcol <- as.vector(meancol + stdcol + idcol)
+
+#Get the desired columns out of the original dataset
+df_with_desiredcol <- combined[,which(as.logical(desiredcol))]
+
+
+#brute force replace all values of activity label with names
+temp <- df_with_desiredcol[,2]
+temp <- replace(temp, temp == 1, "walking")
+temp <- replace(temp, temp == 2, "walking upstairs")
+temp <- replace(temp, temp == 3, "walking downstairs")
+temp <- replace(temp, temp == 4, "sitting")
+temp <- replace(temp, temp == 5, "standing")
+temp <- replace(temp, temp == 6, "laying")
+
+#now replace the column in the dataframe with the new vector
+df_with_desiredcol[,2] <- temp
+
+
+
+#renaming column headers where necessary. Moving all hearders to lowercase, per instructions
+colnames(df_with_desiredcol)[1] <- "SubjectID"
+colnames(df_with_desiredcol)[2] <- "ActivityName"
+colnames(df_with_desiredcol) <- tolower(colnames(df_with_desiredcol))
+
+
+#test
+grpd <- group_by(df_with_desiredcol, subjectid, activityname)
+widetidy <- summarise_each(grpd, funs(mean), -subjectid, -activityname)
+
+
+#then output the file with write file to a text file for upload
+
+write.table(widetidy, file = "KCM_ProjectOutput.txt",row.names = FALSE)
